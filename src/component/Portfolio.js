5571ab3fc38f2e47ef116711920fcaebd87e0071
VaibhavTc <chakolevaibhav17@gmail.com>
2024-10-20 18:30:00 +0530

    updated projects

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBrands } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { faCode, faDuoTone } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faLink } from "@fortawesome/free-solid-svg-icons";
 
 import port1 from "../assets/port1.jpg";
 function PortfolioSection() {
@@ -22,10 +22,21 @@ function PortfolioSection() {
             <img src={port1} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>AI Resume Builder</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/VaibhavTc/buildofy-resume-client"
+                className="icon"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a
+                href="https://buildofy-resume-client.vercel.app/"
+                className="icon"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLink} />
               </a>
             </div>
           </div>
@@ -35,10 +46,21 @@ function PortfolioSection() {
             <img src={port1} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>E-commerce Website</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/VaibhavTc/MERN-Eccommerce"
+                className="icon"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a
+                href="https://mern-eccommerce.vercel.app/"
+                className="icon"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLink} />
               </a>
             </div>
           </div>
@@ -48,10 +70,21 @@ function PortfolioSection() {
             <img src={port1} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>Connectverse</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/VaibhavTc/connectverse"
+                target="_blank"
+                className="icon"
+              >
                 <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a
+                href="https://connectverse-bitt.onrender.com/"
+                className="icon"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLink} />
               </a>
             </div>
           </div>
@@ -61,9 +94,37 @@ function PortfolioSection() {
             <img src={port1} alt="" />
           </div>
           <div className="hover-items">
-            <h3>Project Source</h3>
+            <h3>Spotify Playlist Sorter</h3>
             <div className="icons">
-              <a href="#" className="icon">
+              <a
+                href="https://github.com/VaibhavTc/Spotify-sort"
+                className="icon"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a
+                href="https://spotify-sort.vercel.app/"
+                target="_blank"
+                className="icon"
+              >
+                <FontAwesomeIcon icon={faLink} />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="portfolio-item">
+          <div className="image">
+            <img src={port1} alt="" />
+          </div>
+          <div className="hover-items">
+            <h3>Photo Gallary</h3>
+            <div className="icons">
+              <a
+                href="https://github.com/VaibhavTc/Photo-gallary"
+                className="icon"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
             </div>
